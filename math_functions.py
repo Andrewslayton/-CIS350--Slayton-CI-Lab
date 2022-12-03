f27c67110277068077b6974d5b180c0272c47dbf
Andrew J. Slayton <slaytona@eos12.cis.gvsu.edu>
2022-12-02 19:13:38 -0500

    completed the code for tests

--- math_functions.py
+++ math_functions.py
@@ -6,6 +6,11 @@ def multiply_numbers(a,b):
     return a*b
 def divide_numbers(a,b):
     return a/b
+def power_numbers(a,b):
+    return a**b
+def mult_add_numbers(a,b):
+    answer = (a*b) + a +b
+    return answer
 if __name__ == "__main__":
  print("Adding:", add_numbers(2,4))
  print("Subtracting:", subtract_numbers(9,2))
